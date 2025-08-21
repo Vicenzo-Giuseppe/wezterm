a5b7eed7d12ce02c0157b075f115d133d0fcbb4c
Vicenzo Giuseppe <vicenzogiuseppe@gmail.com>
2025-08-21 14:56:27 -0300

    for compatibility with /nix/store
    modded Configuration for running inside /NIX/STORE 
    - [ folder colors don't auto-generate schemes idk why ]

--- wezterm.lua
+++ wezterm.lua
@@ -1,3 +1,6 @@
+local wezterm = require 'wezterm'
+local config_dir = wezterm.config_dir
+package.path = config_dir .. "/?.lua"
 require('on')
 local config = {
     font = require('wezterm').font('FiraCode Nerd Font'),
@@ -5,6 +8,8 @@ local config = {
     keys = require('keys'),
     key_tables = require('keys_mode'),
     launch_menu = require('utils').launch_menu,
+    colors = wezterm.color.load_scheme(config_dir .. "/colors/catppuccin.toml"), -- in /nix/store cant generate colors from folder
+
 }
 require('utils').translate_toml_to_lua(config)
 return config
